ed3d9fd5d1a55748892d691486d5a7dbbc7bfb3a
tmichoel <11647967+tmichoel@users.noreply.github.com>
2024-04-11 21:11:28 +0200

    update SpatialDE exports

--- src/SpatialOmicsGPs.jl
+++ src/SpatialOmicsGPs.jl
@@ -15,6 +15,6 @@ include("SpatialDE.jl")
 export fastlmm_fullrank, sigma2_mle_fullrank, minus_log_like_fullrank, delta_mle_fullrank, beta_mle_fullrank_lazy, create_covariate_matrix, project_orth_covar, softplus
 
 # SpatialDE exports
-export spatialde_varpars_opt, spatialde_varpars_all, lengthscales
+export spatialde_param_inference, spatialde_varpars_opt, spatialde_varpars_all, lengthscales
 
 end
